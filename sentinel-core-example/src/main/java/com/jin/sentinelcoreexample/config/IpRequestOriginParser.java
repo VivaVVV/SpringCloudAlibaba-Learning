00c2d1aac086b474f998e8dc05ecc880300aeca9
viva <viva@outlook.com>
2020-07-08 21:48:36 +0800

    IP策略实现类

---
+++ sentinel-core-example/src/main/java/com/jin/sentinelcoreexample/config/IpRequestOriginParser.java
@@ -0,0 +1,12 @@
+package com.jin.sentinelcoreexample.config;
+
+import com.alibaba.csp.sentinel.adapter.servlet.callback.RequestOriginParser;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class IpRequestOriginParser implements RequestOriginParser {
+    @Override
+    public String parseOrigin(HttpServletRequest httpServletRequest) {
+        return httpServletRequest.getRemoteAddr();
+    }
+}
